766a272484d7c5769588de77f44f7acc48dcf757
Souvick Chakraborty <hellosouvickk@gmail.com>
2023-04-09 22:39:22 +0530

    counter button Change

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,7 +13,6 @@ const Home = () => {
             <div className="maincomp">
                 <Product />
             </div>
-
         </>
     )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom';
 import { useAuth0 } from "@auth0/auth0-react";
 import { ToastContainer, toast } from 'react-toastify';
